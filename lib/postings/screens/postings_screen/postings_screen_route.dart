3c5bb9748141b657dc30b2be0bd85f0f52ffc570
Joel <joel@promagenviro.com>
2022-08-01 17:33:06 -0700

    added helpers to nav_util

--- lib/postings/screens/postings_screen/postings_screen_route.dart
+++ lib/postings/screens/postings_screen/postings_screen_route.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:joelslist/postings/screens/postings_screen/postings_screen.dart';
 
 class PostingSreenRoute {
+
   static const String routeName = '/postings';
 
   static MaterialPageRoute route(RouteSettings settings) {
